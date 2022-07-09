48b8bfb240053ab870931727b93f6e2cc51e6b6c
Yuliia Khytrikhina <ykhytrukhina@ecisys.com>
2022-07-09 08:13:27 +0300

    configuring prettier, lint

--- models/department.model.js
+++ models/department.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const departmentSchema = mongoose.Schema({
-    id: String,
-    name: { type: String, required: true },
-    description: String,
-    headOfDepartment: { type: String, required: true }
+  id: String,
+  name: { type: String, required: true },
+  description: String,
+  headOfDepartment: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Department', departmentSchema);
--- server.js
+++ server.js
@@ -2,43 +2,43 @@ const app = require('./app');
 const debug = require('debug')('node-angular');
 const http = require('http');
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 };
 
-const onError = error => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? `pipe ${port}` : `port ${port}`;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 
 const onListening = () => {
-    const addr = server.address();
-    const bind = typeof port === 'string' ? 'pipe ' + addr : 'port ' + port;
-    debug('Listening on ' + bind);
+  const addr = server.address();
+  const bind = typeof port === 'string' ? `pipe ${addr}` : `port ${port}`;
+  debug(`Listening on ${bind}`);
 };
 
 const port = normalizePort(process.env.PORT || '3000');
